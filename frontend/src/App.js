482b7b8921aa4432fbc9e7e736c957606d3c7541
Girlie Razon <77394701+girlierazon84@users.noreply.github.com>
2021-10-21 18:46:12 +0200

    Kopierat Frontend mapp från tidigare projektet "Webbserverprogrammering 1"

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import Navbar from './components/Navbar';
+import Input from './components/Input';
+import Footer from './components/Footer';
+
+import './utils/global/css/global.css';
+
+
+function App () {
+
+    return (
+        <div>
+            <Navbar/>
+            <Input/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
